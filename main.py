53dd2ecf66a5743d84cd9f780e52e852fca43c6b
Aman Agarwal <agarwalaman190202@gmail.com>
2025-10-03 14:35:07 -0400

    Trying construction on mac

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import stable_pretraining as spt
 
-train_dataset = spt.data.HFDataset(
-    path="matthieulel/galaxy10_decals",
-    split="train"
-)
 
-print(f"Dataset length: {len(train_dataset)}")
-print(f"Column names: {train_dataset.column_names}")
+if __name__=='__main__':
+
+    train_dataset = spt.data.HFDataset(
+        path="matthieulel/galaxy10_decals",
+        split="train"
+    )
+
+    print(f"Dataset length: {len(train_dataset)}")
+    print(f"Column names: {train_dataset.column_names}")
